aea80986ed1b311fcb0ad41b71b318bb563c0508
sarojsharma1 <sarojsharma11111@gmail.com>
2024-07-13 20:07:18 +0545

    First commit

---
+++ src/main/java/com/mindassist/hami_chau/HamiChauApplication.java
@@ -0,0 +1,13 @@
+package com.mindassist.hami_chau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamiChauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamiChauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindassist/hami_chau/HamiChauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindassist.hami_chau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamiChauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
